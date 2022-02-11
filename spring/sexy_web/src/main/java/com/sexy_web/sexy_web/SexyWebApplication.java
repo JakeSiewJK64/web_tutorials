2591b594aa973cba18812318bdf2899c163dccb2
Siew Joe Kane <joekanesiew@outlook.com>
2022-02-11 16:30:42 +0800

    added spring boot sexy web for testing spring stuff

---
+++ spring/sexy_web/src/main/java/com/sexy_web/sexy_web/SexyWebApplication.java
@@ -0,0 +1,13 @@
+package com.sexy_web.sexy_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SexyWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SexyWebApplication.class, args);
+	}
+
+}
---
+++ spring/sexy_web/src/test/java/com/sexy_web/sexy_web/SexyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sexy_web.sexy_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SexyWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
