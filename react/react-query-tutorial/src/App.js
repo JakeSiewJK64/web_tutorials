c552b1fab4de58737ed156b8cbbe84c287831076
Siew Joe Kane <joekanesiew@outlook.com>
2022-03-10 13:18:49 +0800

    added react project to test react-query

---
+++ react/react-query-tutorial/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+
+import Resistance from "./components/resistance/resistance";
+
+function App() {
+  return (
+    <div className="App">
+      <Resistance />
+    </div>
+  );
+}
+
+export default App;
---
+++ react/react-query-tutorial/src/components/resistance/resistance.js
@@ -0,0 +1,9 @@
+const Resistance = () => {
+  return (
+    <div>
+      <h1>hi</h1>
+    </div>
+  );
+}
+
+export default Resistance;
