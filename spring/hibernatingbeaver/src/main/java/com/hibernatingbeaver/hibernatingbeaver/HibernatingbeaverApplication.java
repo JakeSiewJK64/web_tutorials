137b415003a5a78d2341136fa2cdda875cae8205
Siew Joe Kane <joekanesiew@outlook.com>
2022-03-13 16:14:35 +0800

    added: spring project to learn hibernate ORM

---
+++ spring/hibernatingbeaver/src/main/java/com/hibernatingbeaver/hibernatingbeaver/HibernatingbeaverApplication.java
@@ -0,0 +1,13 @@
+package com.hibernatingbeaver.hibernatingbeaver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HibernatingbeaverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HibernatingbeaverApplication.class, args);
+	}
+
+}
---
+++ spring/hibernatingbeaver/src/test/java/com/hibernatingbeaver/hibernatingbeaver/HibernatingbeaverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hibernatingbeaver.hibernatingbeaver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HibernatingbeaverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
