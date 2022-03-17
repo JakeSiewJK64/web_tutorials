e196802a28eb06dae4f848ca678b35966be98982
Siew Joe Kane <joekanesiew@outlook.com>
2022-03-17 09:56:54 +0800

    update: removed unused Transactional import

--- spring/hibernatingbeaver/src/main/java/com/hibernatingbeaver/example/service/serviceimpl/CategoryServiceImpl.java
+++ spring/hibernatingbeaver/src/main/java/com/hibernatingbeaver/example/service/serviceimpl/CategoryServiceImpl.java
@@ -8,7 +8,6 @@ import com.hibernatingbeaver.example.service.CategoryService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class CategoryServiceImpl implements CategoryService {
