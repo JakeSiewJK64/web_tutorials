afe5566d38b2d0103af842eb6a933fb7c3e7148c
JakeSiewJK64 <joekanesiew@outlook.com>
2025-01-07 16:21:35 +0800

    add tailwind

--- react/react_tracking_analytics/src/pages/Home.jsx
+++ react/react_tracking_analytics/src/pages/Home.jsx
@@ -1,3 +1,10 @@
 export const Home = () => {
-  return <div>home</div>;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <button className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded">
+        Button
+      </button>
+    </div>
+  );
 };
